916cb8367d583a6867ea47a9c1e6e4fa4d9c0e06
Simon MacDonald <simon.macdonald@gmail.com>
2018-01-02 15:03:56 -0500

    ✨ Basic create screen

--- src/components/CreateTabata.js
+++ src/components/CreateTabata.js
@@ -4,6 +4,14 @@ import AppBar from 'material-ui/AppBar';
 import IconButton from 'material-ui/IconButton';
 import NavigationArrowBack from 'material-ui/svg-icons/navigation/arrow-back';
 import PropTypes from 'prop-types';
+import Paper from 'material-ui/Paper';
+import { List } from 'material-ui/List';
+import TextField from 'material-ui/TextField';
+import FlatButton from 'material-ui/FlatButton';
+
+const style = {
+  margin: 20
+};
 
 class CreateTabata extends Component {
   static contextTypes = {
@@ -21,7 +29,32 @@ class CreateTabata extends Component {
                 <NavigationArrowBack />
               </IconButton>
             }
+            iconElementRight={<FlatButton label="Save" />}
           />
+          <Paper style={style} zDepth={1}>
+            <div style={style}>
+              <TextField
+                floatingLabelText="Workout Name"
+                floatingLabelFixed={true}
+              />
+              <TextField
+                floatingLabelText="Warm Up"
+                floatingLabelFixed={true}
+              />
+              <TextField
+                floatingLabelText="Work Out"
+                floatingLabelFixed={true}
+              />
+              <TextField floatingLabelText="Relax" floatingLabelFixed={true} />
+              <TextField floatingLabelText="Break" floatingLabelFixed={true} />
+              <TextField
+                floatingLabelText="Cool Down"
+                floatingLabelFixed={true}
+              />
+              <TextField floatingLabelText="Reps" floatingLabelFixed={true} />
+              <TextField floatingLabelText="Sets" floatingLabelFixed={true} />
+            </div>
+          </Paper>
         </div>
       </MuiThemeProvider>
     );
