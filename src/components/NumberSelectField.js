383cfd9c9f3b49d5e8d683b3353a140257402e19
Simon MacDonald <simon.macdonald@gmail.com>
2018-01-15 16:31:50 -0500

    🔧 Re-order time values

--- src/components/NumberSelectField.js
+++ src/components/NumberSelectField.js
@@ -16,19 +16,19 @@ class NumberSelectField extends SelectField {
         value={this.state.value}
         onChange={this.handleChange}
       >
-        <MenuItem value={60} primaryText="1 minute" />
-        <MenuItem value={55} primaryText="55 seconds" />
-        <MenuItem value={50} primaryText="50 seconds" />
-        <MenuItem value={45} primaryText="45 seconds" />
-        <MenuItem value={40} primaryText="40 seconds" />
-        <MenuItem value={35} primaryText="35 seconds" />
-        <MenuItem value={30} primaryText="30 seconds" />
-        <MenuItem value={25} primaryText="25 seconds" />
-        <MenuItem value={20} primaryText="20 seconds" />
-        <MenuItem value={15} primaryText="15 seconds" />
-        <MenuItem value={10} primaryText="10 seconds" />
-        <MenuItem value={5} primaryText="5 seconds" />
         <MenuItem value={0} primaryText="0 seconds" />
+        <MenuItem value={5} primaryText="5 seconds" />
+        <MenuItem value={10} primaryText="10 seconds" />
+        <MenuItem value={15} primaryText="15 seconds" />
+        <MenuItem value={20} primaryText="20 seconds" />
+        <MenuItem value={25} primaryText="25 seconds" />
+        <MenuItem value={30} primaryText="30 seconds" />
+        <MenuItem value={35} primaryText="35 seconds" />
+        <MenuItem value={40} primaryText="40 seconds" />
+        <MenuItem value={45} primaryText="45 seconds" />
+        <MenuItem value={50} primaryText="50 seconds" />
+        <MenuItem value={55} primaryText="55 seconds" />
+        <MenuItem value={60} primaryText="1 minute" />
       </SelectField>
     );
   }
